8fd0abe8c4bb23e1a6eefb45e7680ca2cd473ede
ffse1702026 <nguyenxuansangbkdn@gmail.com>
2020-10-24 19:49:18 +0700

    update

--- android/copy_build_extra.js
+++ android/copy_build_extra.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
-
 const fs = require('fs')
 // var mkdirs = require('mkdirs');
 fs.copyFileSync("./build-extras.gradle", "./platforms/android/build-extras.gradle");
 fs.copyFileSync("./gradle.properties", "./platforms/android/gradle.properties");
 // fs.copyFileSync("./ExitApp.java", "./platforms/android/app/src/main/java/org/apache/cordova/exitapp/ExitApp.java");
 fs.copyFileSync("./InAppBrowser.java", "./platforms/android/app/src/main/java/org/apache/cordova/inappbrowser/InAppBrowser.java");
-dir = "./platforms/android/platform_www/plugins/com.xxeample.com/www"
+dir = "./platforms/android/platform_www/plugins/com.msopentech.authdialog/www"
 if (!fs.existsSync(dir)){
     fs.mkdirSync(dir, { recursive: true });
-    fs.copyFileSync("./InAppBrowser.java", "./platforms/android/platform_www/plugins/com.xeample.com/InAppBrowser.java");
 }
-
+fs.copyFileSync("./authDialog.js", `${dir}/authDialog.js`);
+fs.copyFileSync("./cordova_plugins.js", `./platforms/android/platform_www/cordova_plugins.js`);
+fs.copyFileSync("./authdialog-strings.xml", `./platforms/android/app/src/main/res/values/authdialog-strings.xml`);
+fs.copyFileSync("./AuthRequestHandler.java", `./platforms/android/app/src/main/java/com/msopentech/authDialog/AuthRequestHandler.java`);
